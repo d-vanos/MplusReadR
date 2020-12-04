a96f775cddc1654cffc62807cccd59854deb4e42
d-vanos <danielle.vanos@unimelb.edu.au>
2020-12-04 11:31:36 +1000

    fixes filenames before import
    MplusAutomation replaces any special characters in file names with full stops, which can interfere with dataset_title specification. This function replaces these special characters with underscores so they can be read in correctly.

---
+++ R/fix_file_names.R
@@ -0,0 +1,29 @@
+
+#' Fix File Names
+#'
+#' The [MplusAutomation::readModels()] function from the `MplusAutomation` package reads all special characters in file names, such as
+#' brackets, dashes, and spaces, in as full stops. This is problematic for determining the name of the file. This function replaces
+#' all special characters in file names with underscores, renaming the files. These can then be read in correctly by [MplusAutomation::readModels()].
+#' Warning: it is recommended that you back up the files before renaming
+#'
+#' @import readr
+#' @param dir A directory
+#' @return Renamed files
+#' @export
+
+fix_file_names <- function(dir){
+
+  # Download the files from the right directory
+  old_file_names = list.files(dir)
+  #files = lapply(paste0(dir, "/", old_file_names), read.delim)
+
+  # Replace any cases of full stops, dashes, spaces or brackets
+  new_file_names <- gsub('-|\\(|\\)| ', '_', old_file_names)
+
+  # Write files (if any changes should be made)
+  if(length(setdiff(new_file_names, old_file_names)) > 0){
+    mapply(file.rename, paste0(dir, "/", old_file_names), paste0(dir, "/", new_file_names))
+  }
+}
+
+
